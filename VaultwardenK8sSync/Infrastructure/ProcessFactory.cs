af2106af1651b26e94f75a0cbc7034d88a87480f
Antônio Lago <45375617+antoniolago@users.noreply.github.com>
2026-02-02 13:36:56 -0300

    Create ProcessFactory.cs

---
+++ VaultwardenK8sSync/Infrastructure/ProcessFactory.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+
+namespace VaultwardenK8sSync.Infrastructure;
+
+public interface IProcessFactory
+{
+    Process CreateBwProcess(string arguments);
+}
+
+public class ProcessFactory : IProcessFactory
+{
+    public Process CreateBwProcess(string arguments)
+    {
+        return new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "bw",
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+    }
+}
